b12ae8caef514967bf713cbf2a535c7e9ede4fe7
John Dagdelen <jdagdelen@users.noreply.github.com>
2020-02-11 02:31:04 -0800

    Update main.py
    fixing mixed tabs and spaces

--- face_parsing/main.py
+++ face_parsing/main.py
@@ -12,9 +12,9 @@ def main(config):
     if config.train:
 
 	# Create directories if not exist
-    	make_folder(config.model_save_path, config.version)
-    	make_folder(config.sample_path, config.version)
-    	make_folder(config.log_path, config.version)
+        make_folder(config.model_save_path, config.version)
+        make_folder(config.sample_path, config.version)
+        make_folder(config.log_path, config.version)
 	
         data_loader = Data_Loader(config.img_path, config.label_path, config.imsize,
                              config.batch_size, config.train)
